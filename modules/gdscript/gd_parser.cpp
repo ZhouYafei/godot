fd754d81d38a5134839326ad6c6ba30190698229
sanikoyes <sanikoyes@163.com>
2016-06-30 15:51:40 +0800

    null and object cannot be constant-node

--- modules/gdscript/gd_parser.cpp
+++ modules/gdscript/gd_parser.cpp
@@ -505,12 +505,7 @@ GDParser::Node* GDParser::_parse_expression(Node *p_parent,bool p_static,bool p_
 			if (GDScriptLanguage::get_singleton()->get_global_map().has(identifier)) {
 				//check from constants
 				Variant value = GDScriptLanguage::get_singleton()->get_global_array()[ GDScriptLanguage::get_singleton()->get_global_map()[identifier] ];
-				bool is_script_object = false;
-				if(value.get_type() == Variant::OBJECT && !((Object *) value)->get_script().is_null())
-					is_script_object= true;
-
-				// script object cannot be constant-node
-				if(!is_script_object && value.get_type() != Variant::NIL) {
+				if(value.get_type() != Variant::NIL && value.get_type() != Variant::OBJECT) {
 
 					ConstantNode *constant = alloc_node<ConstantNode>();
 					constant->value = value;
