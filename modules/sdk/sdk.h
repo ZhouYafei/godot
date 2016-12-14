f56a9b24bed946883abb7cdf7673a2717db0e37c
Gok <gok@GokdeMac-mini.local>
2016-12-14 17:04:59 +0800

    ios fix

--- modules/sdk/sdk.h
+++ modules/sdk/sdk.h
@@ -41,7 +41,7 @@ class Sdk : public Object {
 
 	static Sdk* instance;
 
-	void on_result(int p_code, const String& p_msg);
+	virtual void on_result(int p_code, const String& p_msg);
 
 protected:
 	void sendCallback(const String p_what, const Dictionary& p_data = Dictionary());
@@ -49,34 +49,34 @@ protected:
 	static void _bind_methods();
 
 public:
-	void init(int p_handler, const String& p_callback);
-	bool is_support(const String& p_plugin, const String& p_what);
-	int get_curr_channel();
-	int get_logic_channel();
-	int get_app_id();
-	String get_app_key();
-	String get_app_signature();
-	void tip(const String& p_tip);
-	String get_network_type();
-	void login();
-	void login_custom(const String& p_extension);
-	void switch_login();
-	void logout();
-	void show_user_center();
-	void submit_extra(const Dictionary& p_data);
-	void exit();
-	void pay(const Dictionary& p_data);
-	void start_push();
-	void stop_push();
-	void add_tags(const StringArray& p_tags);
-	void remove_tags(const StringArray& p_tags);
-	void add_alias(const String& p_alias);
-	void remove_alias(const String& p_alias);
-	void share(const Dictionary& p_data);
-	void analytics(const Dictionary& p_data);
-	void download(const String& p_url, bool p_show_confirm, bool p_force);
-	String get_clipboard();
-	void set_clipboard(const String& p_text);
+	virtual void init(int p_handler, const String& p_callback);
+	virtual bool is_support(const String& p_plugin, const String& p_what);
+	virtual int get_curr_channel();
+	virtual int get_logic_channel();
+	virtual int get_app_id();
+	virtual String get_app_key();
+	virtual String get_app_signature();
+	virtual void tip(const String& p_tip);
+	virtual String get_network_type();
+	virtual void login();
+	virtual void login_custom(const String& p_extension);
+	virtual void switch_login();
+	virtual void logout();
+	virtual void show_user_center();
+	virtual void submit_extra(const Dictionary& p_data);
+	virtual void exit();
+	virtual void pay(const Dictionary& p_data);
+	virtual void start_push();
+	virtual void stop_push();
+	virtual void add_tags(const StringArray& p_tags);
+	virtual void remove_tags(const StringArray& p_tags);
+	virtual void add_alias(const String& p_alias);
+	virtual void remove_alias(const String& p_alias);
+	virtual void share(const Dictionary& p_data);
+	virtual void analytics(const Dictionary& p_data);
+	virtual void download(const String& p_url, bool p_show_confirm, bool p_force);
+	virtual String get_clipboard();
+	virtual void set_clipboard(const String& p_text);
 
 	static Sdk* get_singleton();
 
