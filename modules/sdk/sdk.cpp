0224d9fd5f89f141899f432dbcd92e3f28ba2dfe
Gok <gok@GokdeMac-mini.local>
2016-12-15 15:26:17 +0800

    ios sdk

--- modules/sdk/sdk.cpp
+++ modules/sdk/sdk.cpp
@@ -31,51 +31,6 @@
 #include "sdk.h"
 #include "os/os.h"
 
-static const char *CALLBACK_INIT = "init";
-static const char *CALLBACK_LOGIN = "login";
-static const char *CALLBACK_SWITCH_LOGIN = "switch_login";
-static const char *CALLBACK_LOGOUT = "logout";
-static const char *CALLBACK_PAY = "pay";
-static const char *CALLBACK_RESULT = "result";
-
-enum U8Codes {
-	CODE_NO_NETWORK = 0,			// 没有网络连接
-	CODE_INIT_SUCCESS = 1,			// 初始化成功
-	CODE_INIT_FAIL = 2,				// 初始化失败
-	CODE_UNINIT = 3,				// 没有初始化
-	CODE_LOGIN_SUCCESS = 4,			// 登录成功
-	CODE_LOGIN_FAIL = 5,			// 登录失败
-	CODE_LOGIN_TIMEOUT = 6,			// 登录超时
-	CODE_UNLOGIN = 7,				// 没有登录
-	CODE_LOGOUT_SUCCESS = 8,		// 登出成功
-	CODE_LOGOUT_FAIL = 9,			// 登出失败
-	CODE_PAY_SUCCESS = 10,			// 支付成功
-	CODE_PAY_FAIL = 11,				// 支付失败
-	CODE_TAG_ADD_SUC = 12,			// 添加Tag成功
-	CODE_TAG_ADD_FAIL = 13,			// 添加Tag失败
-	CODE_TAG_DEL_SUC = 14,			// 删除Tag成功
-	CODE_TAG_DEL_FAIL = 15,			// 删除Tag失败
-	CODE_ALIAS_ADD_SUC = 16,		// 添加Alias成功
-	CODE_ALIAS_ADD_FAIL = 17,		// 添加Alias失败
-	CODE_ALIAS_REMOVE_SUC = 18,		// 删除Alias成功
-	CODE_ALIAS_REMOVE_FAIL = 19,	// 删除Alias失败
-	CODE_PUSH_MSG_RECIEVED = 20,	// Push 收到msg
-	CODE_PARAM_ERROR = 21,			// 参数 错误
-	CODE_PARAM_NOT_COMPLETE = 22,	// 参数不全
-	CODE_SHARE_SUCCESS = 23,		// 分享成功
-	CODE_SHARE_FAILED = 24,			// 分享失败
-	CODE_UPDATE_SUCCESS = 25,		// 更新成功
-	CODE_UPDATE_FAILED = 26,		// 更新失败
-	CODE_REAL_NAME_REG_SUC = 27,	// 实名注册成功
-	CODE_REAL_NAME_REG_FAILED = 28,	// 实名注册失败
-	CODE_ADDICTION_ANTI_RESULT = 29,// 房沉迷查询结果
-	CODE_PUSH_ENABLED = 30,			// 推送enable成功的回调，携带一个参数，比如友盟推送，这参数是Device Token
-	CODE_POST_GIFT_SUC = 31,		// 提交礼包兑换码成功
-	CODE_POST_GIFT_FAILED = 32,		// 提交礼包兑换码失败
-	CODE_PAY_CANCEL = 33,			// 取消支付
-	CODE_PAY_UNKNOWN = 34,			// 支付未知
-	CODE_PAYING = 35,				// 支付中
-};
 static const char *U8CodeStrings[] = {
 	"no_network",
 	"init_success",
